451be112bb878e0c09e550cfe9c0e927129966dd
Lionel Mann <lionelmann@gmail.com>
2017-11-23 14:33:04 -0500

    footer and header

--- src/components/Shared/Header.vue
+++ src/components/Shared/Header.vue
@@ -1,9 +1,21 @@
 <template>
-	
+    <section class="hero is-success">
+        <div class="hero-body">
+            <div class="container">
+            <h1 class="title">
+                Park People
+            </h1>
+            <h2 class="subtitle">
+                Summary
+            </h2>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
 	export default {
+		
 	};
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { routes } from './routes'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+
+// Use
+Vue.use(Buefy);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+	routes, // Have to pass an object where we setup the router. ES6 syntax (routes:routes)
+	mode: 'history' // Remove the hash
+})
 
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 })
