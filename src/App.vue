997215861f143013c1ca6625d9fe24647ae202e5
Lionel Mann <lionelmann@gmail.com>
2017-12-19 14:25:55 -0500

    transition

--- src/App.vue
+++ src/App.vue
@@ -77,11 +77,12 @@ small {
     font-size: 0.815rem;
 }
 
-.fade-enter-active, .fade-leave-active {
-    transition: opacity .3s
+.fade-enter-active {
+    transition: opacity .5s
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-    opacity: 0
+
+.fade-leave-active {
+    transition: opacity .5s
 }
 
 .wfs-loading {
